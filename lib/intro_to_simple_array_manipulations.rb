2e8332f917ec9e97b92323f4a3a0bd52e581b6c3
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-04 00:20:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -71,7 +71,7 @@ end
 
 def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  instructors.delete("Steven")
+  array.delete("string")
 end
 
 #.delete_if{|i|i==6}
